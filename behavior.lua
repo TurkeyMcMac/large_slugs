366742955522c87e78e986e3cab08cf189cbaa1c
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-09-24 21:49:05 -0400

    Improve slug updates
    This commit includes some optimizations. These are probably not
    significant, but since this mod provides a minor feature, it should be
    as performant as possible in order to justify its existence.

--- behavior.lua
+++ behavior.lua
@@ -1,14 +1,20 @@
-local MOVE_INTERVAL =
-	tonumber(minetest.settings:get("large_slugs_move_interval")) or 5
-local MOVE_CHANCE =
-	tonumber(minetest.settings:get("large_slugs_move_chance")) or 5
-local BIRTH_INTERVAL =
-	tonumber(minetest.settings:get("large_slugs_birth_interval")) or 10
+local UPDATE_INTERVAL =
+	tonumber(minetest.settings:get("large_slugs_update_interval")) or 5
+local UPDATE_CHANCE =
+	tonumber(minetest.settings:get("large_slugs_update_chance")) or 5
 local BIRTH_CHANCE =
-	tonumber(minetest.settings:get("large_slugs_birth_chance")) or 10
+	tonumber(minetest.settings:get("large_slugs_birth_chance")) or 5
 local BIRTH_SPACE =
 	tonumber(minetest.settings:get("large_slugs_birth_space")) or 11
 
+-- With the given vectors, computes a = b + c.
+local function set_add(a, b, c)
+	a.x = b.x + c.x
+	a.y = b.y + c.y
+	a.z = b.z + c.z
+end
+
+-- A map of wallmounted directions to their perpendicular directions.
 local WALLMOUNT_TO_PERP_WALLMOUNTS = {}
 WALLMOUNT_TO_PERP_WALLMOUNTS[0] = {4, 2, 5, 3}
 WALLMOUNT_TO_PERP_WALLMOUNTS[2] = {4, 0, 5, 1}
@@ -17,109 +23,89 @@ WALLMOUNT_TO_PERP_WALLMOUNTS[1] = WALLMOUNT_TO_PERP_WALLMOUNTS[0]
 WALLMOUNT_TO_PERP_WALLMOUNTS[3] = WALLMOUNT_TO_PERP_WALLMOUNTS[2]
 WALLMOUNT_TO_PERP_WALLMOUNTS[5] = WALLMOUNT_TO_PERP_WALLMOUNTS[4]
 
+-- A map of wallmounted directions to their opposites.
 local WALLMOUNT_TO_OPP_WALLMOUNT = {
 	[0] = 1, [1] = 0, [2] = 3, [3] = 2, [4] = 5, [5] = 4,
 }
 
-local function get_move_options(pos, node, include_here)
-	local ground_nodes = large_slugs.registered_slugs[node.name].ground
+-- The numbers added to movement added wallmounted values to identify the type
+-- of movement:
+-- 1. Shifting to an adjacent node without rotation.
+-- 2. Rotating to attach to a different node around the same position.
+-- 3. Rotating around the node to which the slug is attached.
+local SHIFT = 32
+local ROTATE = 64
+local SHIFT_ROTATE = 96
 
-	local wallmount = node.param2 % 6
-	local dir = minetest.wallmounted_to_dir(wallmount)
+-- These are kept around to save a few allocations, probably negligible.
+local opts = {}
+local check_pos = {}
 
-	local rotate_opts = include_here and {}
-	local shift_opts = {}
-	local rotate_shift_opts = {}
+-- Update a slug, randomly trying to either move it or have it give birth.
+local function update_slug(pos, node)
+	local def = large_slugs.registered_slugs[node.name]
+	if not def then return end
 
-	local perp_wallmounts = WALLMOUNT_TO_PERP_WALLMOUNTS[wallmount]
+	local old_wallmount = node.param2
+	local old_dir = minetest.wallmounted_to_dir(old_wallmount)
+	if not old_dir then return end
+
+	-- Determine whether this is a move or a birth (births require an empty
+	-- area around the parent):
+	local move = math.random(BIRTH_CHANCE) < BIRTH_CHANCE or
+		minetest.find_node_near(pos, BIRTH_SPACE, node.name) ~= nil
+
+	-- Scan for options, looking in directions perpendicular to the current
+	-- wallmounted direction of the node.
+	local n_opts = 0
+	local perp_wallmounts = WALLMOUNT_TO_PERP_WALLMOUNTS[old_wallmount]
 	for _, perp_wallmount in ipairs(perp_wallmounts) do
-		local perp_dir = minetest.wallmounted_to_dir(perp_wallmount)
-
-		local check_pos = vector.add(pos, perp_dir)
+		set_add(check_pos,
+			pos, minetest.wallmounted_to_dir(perp_wallmount))
 		local check_node = minetest.get_node(check_pos)
-		if include_here and ground_nodes[check_node.name] then
-			rotate_opts[#rotate_opts + 1] = perp_wallmount
+		if move and def.ground[check_node.name] then
+			n_opts = n_opts + 1
+			opts[n_opts] = perp_wallmount + ROTATE
 		elseif check_node.name == "air" then
-			local shift_pos = check_pos
-			check_pos = vector.add(check_pos, dir)
+			set_add(check_pos, check_pos, old_dir)
 			check_node = minetest.get_node(check_pos)
-			if ground_nodes[check_node.name] then
-				shift_opts[#shift_opts + 1] = shift_pos
+			if def.ground[check_node.name] then
+				n_opts = n_opts + 1
+				opts[n_opts] = perp_wallmount + SHIFT
 			elseif check_node.name == "air" then
-				rotate_shift_opts[#rotate_shift_opts + 1] = {
-					WALLMOUNT_TO_OPP_WALLMOUNT[
-						perp_wallmount],
-					check_pos,
-				}
+				n_opts = n_opts + 1
+				opts[n_opts] = perp_wallmount + SHIFT_ROTATE
 			end
 		end
 	end
 
-	return rotate_opts, shift_opts, rotate_shift_opts
-end
-
-local function move_slug(pos, node)
-	local rotate_opts, shift_opts, rotate_shift_opts =
-		get_move_options(pos, node, true)
-
-	local n_rotate_opts = #rotate_opts
-	local n_shift_opts = #shift_opts
-	local n_rotate_shift_opts = #rotate_shift_opts
-	local n_opts = n_rotate_opts + n_shift_opts + n_rotate_shift_opts
 	if n_opts < 1 then return end
-	local choice = math.random(n_opts)
-	if choice <= n_rotate_opts then
-		local chosen_wallmount = rotate_opts[choice]
-		node.param2 = node.param2 - node.param2 % 8 + chosen_wallmount
+
+	-- Choose one of the options and move in the way it specifies.
+	local chosen = opts[math.random(n_opts)]
+	local new_wallmount = chosen % 8
+	local chosen_type = chosen - new_wallmount
+	if chosen_type == SHIFT then
+		if move then minetest.remove_node(pos) end
+		set_add(pos, pos, minetest.wallmounted_to_dir(new_wallmount))
+		minetest.set_node(pos, node)
+	elseif chosen_type == ROTATE then
+		node.param2 = new_wallmount
 		minetest.swap_node(pos, node)
-	elseif choice <= n_rotate_opts + n_shift_opts then
-		local chosen_pos = shift_opts[choice - n_rotate_opts]
-		minetest.remove_node(pos)
-		minetest.set_node(chosen_pos, node)
-	else
-		local chosen =
-			rotate_shift_opts[choice - n_rotate_opts - n_shift_opts]
-		minetest.remove_node(pos)
-		node.param2 = node.param2 - node.param2 % 8 + chosen[1]
-		minetest.set_node(chosen[2], node)
-	end
-end
-
-local function birth_slug(pos, node)
-	if minetest.find_node_near(pos, BIRTH_SPACE, node.name) then return end
-
-	local _rotate_opts, shift_opts, rotate_shift_opts =
-		get_move_options(pos, node, false)
-
-	local n_shift_opts = #shift_opts
-	local n_rotate_shift_opts = #rotate_shift_opts
-	local n_opts = n_shift_opts + n_rotate_shift_opts
-	if n_opts < 1 then return end
-	local choice = math.random(n_opts)
-	if choice <= n_shift_opts then
-		local chosen_pos = shift_opts[choice]
-		minetest.set_node(chosen_pos, node)
-	else
-		local chosen = rotate_shift_opts[choice - n_shift_opts]
-		node.param2 = node.param2 - node.param2 % 8 + chosen[1]
-		minetest.set_node(chosen[2], node)
+	else -- SHIFT_ROTATE
+		if move then minetest.remove_node(pos) end
+		set_add(pos, pos, minetest.wallmounted_to_dir(new_wallmount))
+		set_add(pos, pos, old_dir)
+		node.param2 = WALLMOUNT_TO_OPP_WALLMOUNT[new_wallmount]
+		minetest.set_node(pos, node)
 	end
 end
 
 minetest.register_abm({
-	label = "Slugs moving",
+	label = "Slugs updating",
 	nodenames = "group:large_slug",
-	interval = MOVE_INTERVAL,
-	chance = MOVE_CHANCE,
+	interval = UPDATE_INTERVAL,
+	chance = UPDATE_CHANCE,
 	catch_up = false,
-	action = move_slug,
-})
-
-minetest.register_abm({
-	label = "Slugs birthing",
-	nodenames = "group:large_slug",
-	interval = BIRTH_INTERVAL,
-	chance = BIRTH_CHANCE,
-	catch_up = false,
-	action = birth_slug,
+	action = update_slug,
 })
