e5800e246aa505f88e096de3a21f50892900be8a
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-09-25 11:06:42 -0400

    Allow disabling of behavior

--- init.lua
+++ init.lua
@@ -1,5 +1,11 @@
 local modpath = minetest.get_modpath(minetest.get_current_modname())
+
 dofile(modpath.."/api.lua")
-dofile(modpath.."/behavior.lua")
+
+if minetest.settings:get_bool("large_slugs_do_behavior", true) then
+	dofile(modpath.."/behavior.lua")
+end
+
 dofile(modpath.."/slugs.lua")
+
 dofile(modpath.."/mapgen.lua")
