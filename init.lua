cf38d4ded1a00ff8baf3e7451351fb2940838b2f
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-10-03 12:39:31 -0400

    Use spaces around concatenation operators

--- init.lua
+++ init.lua
@@ -19,12 +19,12 @@
 
 local modpath = minetest.get_modpath(minetest.get_current_modname())
 
-dofile(modpath.."/api.lua")
+dofile(modpath .. "/api.lua")
 
 if minetest.settings:get_bool("large_slugs_do_behavior", true) then
-	dofile(modpath.."/behavior.lua")
+	dofile(modpath .. "/behavior.lua")
 end
 
-dofile(modpath.."/slugs.lua")
+dofile(modpath .. "/slugs.lua")
 
-dofile(modpath.."/mapgen.lua")
+dofile(modpath .. "/mapgen.lua")
